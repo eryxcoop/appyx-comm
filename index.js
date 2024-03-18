c9d22b11c0dd6837fa18ef96267ce4294874cb88
Florencia-97 <florrr1997@gmail.com>
2024-03-18 10:11:17 -0300

    Adds new parameters to package.json

--- index.js
+++ index.js
@@ -8,14 +8,14 @@ export {default as AuthorizationManager} from "./src/authorization/Authorization
 
 // Responses
 export {default as ApiResponse} from "./src/responses/ApiResponse";
-export {default as SuccessfulApiResponse} from  "./src/responses/generalResponses/SuccessfulApiResponse";
-export {default as ApiErrorResponse} from  "./src/responses/generalResponses/ApiErrorResponse";
-export {default as UnexpectedErrorResponse} from  "./src/responses/generalResponses/UnexpectedErrorResponse";
-export {default as AuthenticationErrorResponse} from  "./src/responses/generalResponses/AuthenticationErrorResponse";
-export {default as InvalidCredentialsResponse} from  "./src/responses/generalResponses/InvalidCredentialsResponse";
-export {default as SimpleApiErrorResponse} from  "./src/responses/generalResponses/SimpleApiErrorResponse";
-export {default as PermissionErrorResponse} from  "./src/responses/generalResponses/PermissionErrorResponse";
-export {default as ServerErrorResponse} from  "./src/responses/generalResponses/ServerErrorResponse";
+export {default as SuccessfulApiResponse} from "./src/responses/generalResponses/SuccessfulApiResponse";
+export {default as ApiErrorResponse} from "./src/responses/generalResponses/ApiErrorResponse";
+export {default as UnexpectedErrorResponse} from "./src/responses/generalResponses/UnexpectedErrorResponse";
+export {default as AuthenticationErrorResponse} from "./src/responses/generalResponses/AuthenticationErrorResponse";
+export {default as InvalidCredentialsResponse} from "./src/responses/generalResponses/InvalidCredentialsResponse";
+export {default as SimpleApiErrorResponse} from "./src/responses/generalResponses/SimpleApiErrorResponse";
+export {default as PermissionErrorResponse} from "./src/responses/generalResponses/PermissionErrorResponse";
+export {default as ServerErrorResponse} from "./src/responses/generalResponses/ServerErrorResponse";
 
 
 // Client
